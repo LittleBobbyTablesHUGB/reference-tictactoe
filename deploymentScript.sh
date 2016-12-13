801d413b49ed421aaff77905e5d079c58fc952ca
FanneyA <fanney13@ru.is>
2016-12-13 09:40:05 -0500

    Minor changes to script

--- deploymentScript.sh
+++ deploymentScript.sh
@@ -5,9 +5,10 @@ scp -o StrictHostKeyChecking=no -i "TicTacKey.pem" ./archive/docker-compose.yaml
 :~/docker-compose.yaml
 
 
-scp -o StrictHostKeyChecking=no -i "TicTacKey.pem" ./archive/docker-compose-and-run.sh $
-:~/docker-compose-and-run.sh
+scp -o StrictHostKeyChecking=no -i "TicTacKey.pem" ./archive/.env $
+:~/.env
 
 
 echo 'Connect to AWS'
 ssh -i "TicTacKey.pem" ec2-user@ec2-54-213-255-232.us-west-2.compute.amazonaws.com
+docker-compose up -d
