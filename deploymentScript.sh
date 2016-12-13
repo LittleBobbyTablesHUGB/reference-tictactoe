90cc15c51a1f1f2438cad1802903ae07a1799119
FanneyA <fanney13@ru.is>
2016-12-13 10:24:31 -0500

    Minor changes to deploymentScript.sh

--- deploymentScript.sh
+++ deploymentScript.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 ls ./archive -a
 echo 'Copy Docker Compose to AWS'
-scp -o StrictHostKeyChecking=no -i "TicTacKey.pem" ./archive/docker-compose.yaml ec2-user@ec2-54-213-255-232.us-west-2.compute.amazonaws.com:~/.
+scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/workspace/Deployment/TicTacKey.pem" ./archive/docker-compose.yaml ec2-user@ec2-54-213-255-232.us-west-2.compute.amazonaws.com:~/.
 
-scp -o StrictHostKeyChecking=no -i "TicTacKey.pem" ./archive/.env ec2-user@ec2-54-213-255-232.us-west-2.compute.amazonaws.com:~/.
+scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/workspace/Deployment/TicTacKey.pem" ./archive/.env ec2-user@ec2-54-213-255-232.us-west-2.compute.amazonaws.com:~/.
 
 echo 'Connect to AWS'
-ssh -i "TicTacKey.pem" ec2-user@ec2-54-213-255-232.us-west-2.compute.amazonaws.com
+ssh -i "/var/lib/jenkins/workspace/Deployment/TicTacKey.pem" ec2-user@ec2-54-213-255-232.us-west-2.compute.amazonaws.com
 docker-compose up -d
 exit
 echo 'DONE'
