dd3bec45c58e8b30a8026014f85303170a6bfe3a
FanneyA <fanney13@ru.is>
2016-12-13 11:10:14 -0500

    Minor changes to script

--- deploymentScript.sh
+++ deploymentScript.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-ls ./archive -a
 echo 'Copy Docker Compose to AWS'
-scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/workspace/Deployment/TicTacKey.pem" ./archive/docker-compose.yaml ec2-user@ec2-54-213-255-232.us-west-2.compute.amazonaws.com:~/.
+scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/workspace/Deployment/TicTacKey.pem" ./docker-compose.yaml ec2-user@ec2-54-213-255-232.us-west-2.compute.amazonaws.com:~/.
 
-scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/workspace/Deployment/TicTacKey.pem" ./archive/.env ec2-user@ec2-54-213-255-232.us-west-2.compute.amazonaws.com:~/.
+scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/workspace/Deployment/TicTacKey.pem" ./.env ec2-user@ec2-54-213-255-232.us-west-2.compute.amazonaws.com:~/.
 
 echo 'Connect to AWS'
 ssh -i "/var/lib/jenkins/workspace/Deployment/TicTacKey.pem" ec2-user@ec2-54-213-255-232.us-west-2.compute.amazonaws.com "docker-compose up -d"
