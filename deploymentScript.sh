4a00734b7be95079bc4ef5fda0a2952ca5ef59be
FanneyA <fanney13@ru.is>
2016-12-14 09:34:24 -0500

    Minor changes to path in script2

--- deploymentScript.sh
+++ deploymentScript.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo 'Copy Docker Compose to AWS'
-scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/workspace/Deployment/build/TicTacKey.pem" /var/lib/jenkins/workspace/TicTacToe-Commit/build/docker-compose.yaml ec2-user@ec2-54-213-255-232.us-west-2.compute.amazonaws.com:~/docker-compose.yaml
+scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/workspace/Deployment/TicTacKey.pem" /var/lib/jenkins/workspace/TicTacToe-Commit/build/docker-compose.yaml ec2-user@ec2-54-213-255-232.us-west-2.compute.amazonaws.com:~/docker-compose.yaml
 
-scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/workspace/Deployment/build/TicTacKey.pem" /var/lib/jenkins/workspace/TicTacToe-Commit/build/.env ec2-user@ec2-54-213-255-232.us-west-2.compute.amazonaws.com:~/.env
+scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/workspace/Deployment/TicTacKey.pem" /var/lib/jenkins/workspace/TicTacToe-Commit/build/.env ec2-user@ec2-54-213-255-232.us-west-2.compute.amazonaws.com:~/.env
 
 echo 'Connect to AWS'
 ssh -i "/var/lib/jenkins/workspace/Deployment/TicTacKey.pem" ec2-user@ec2-54-213-255-232.us-west-2.compute.amazonaws.com "docker-compose up -d"
