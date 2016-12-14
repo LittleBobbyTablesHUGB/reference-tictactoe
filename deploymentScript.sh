b1dd36b2bfcc7fcda5c6f267b3adb092908a822d
FanneyA <fanney13@ru.is>
2016-12-14 05:03:07 -0500

    Minor changes to deploymentScript.sh

--- deploymentScript.sh
+++ deploymentScript.sh
@@ -5,7 +5,7 @@ scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/workspace/Deployment/TicTac
 scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/workspace/Deployment/TicTacKey.pem" ./var/lib/jenkins/workspace/TicTacToe-Commit/build/.env ec2-user@ec2-54-213-255-232.us-west-2.compute.amazonaws.com:~/
 
 echo 'Connect to AWS'
-ssh -i "/var/lib/jenkins/workspace/Deployment/TicTacKey.pem" ec2-user@ec2-54-213-255-232.us-west-2.compute.amazonaws.com
-docker-compose up -d
+ssh -i "/var/lib/jenkins/workspace/Deployment/TicTacKey.pem" ec2-user@ec2-54-213-255-232.us-west-2.compute.amazonaws.com "docker-compose up -d"
+
 exit
 echo 'DONE'
