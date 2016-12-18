e7109be0849846449728e51c45800d832dae846e
FanneyA <fanney13@ru.is>
2016-12-18 13:27:18 -0500

    Adding Column to db

--- deploymentScript.sh
+++ deploymentScript.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-echo 'Copy Docker Compose to AWS'
+# Copies Docker Compose to AWS
 scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/workspace/Deployment/TicTacKey.pem" /var/lib/jenkins/workspace/TicTacToe-Commit/build/docker-compose.yaml ec2-user@ec2-54-213-255-232.us-west-2.compute.amazonaws.com:~/docker-compose.yaml
 
 scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/workspace/Deployment/TicTacKey.pem" /var/lib/jenkins/workspace/TicTacToe-Commit/build/.env ec2-user@ec2-54-213-255-232.us-west-2.compute.amazonaws.com:~/.env
 
-echo 'Connect to AWS'
+#Connects to AWS
 ssh -i "/var/lib/jenkins/workspace/Deployment/TicTacKey.pem" ec2-user@ec2-54-213-255-232.us-west-2.compute.amazonaws.com "docker-compose up -d"
 exit
-echo 'DONE'
